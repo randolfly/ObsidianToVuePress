65c84ece536f4e0bf9edc778b8eff0589f8979f6
张荣侨 <zrq@mails.tsinghua.edu.cn>
2022-08-08 17:27:37 +0800

    init the project

---
+++ ObsidianToVuePress/Context/FileConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObsidianToVuePress.Context
+{
+    public class FileConfig
+    {
+        /// <summary>
+        /// obsidian vault 文件夹路径
+        /// </summary>
+        public string VaultPath { get; set; }
+
+        /// <summary>
+        /// 输出目标文件夹地址
+        /// </summary>
+        public string DestinationPath { get; set; }
+        //Todo: 添加文件过滤选项
+        /// <summary>
+        /// 忽略的文件路径
+        /// </summary>
+        public List<String> IgnorePaths { get; set; }
+
+        /// <summary>
+        /// 强行添加的文件路径
+        /// </summary>
+        public List<String> AddPaths { get; set; }
+    }
+}
---
+++ ObsidianToVuePress/Shared/ObsidianSystemInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObsidianToVuePress.Shared
+{
+    public class ObsidianSystemInfo
+    {
+        // TODO: Change this to config outside
+        /// <summary>
+        /// Log文件地址
+        /// </summary>
+        public static string LogPath = "log.txt";
+        /// <summary>
+        /// Config文件地址
+        /// </summary>
+        public static string ConfigPath = "config.yml";
+        /// <summary>
+        /// 存储数据的数据库
+        /// </summary>
+        public static string DbPath = "obsidianDb.db";
+    }
+}
